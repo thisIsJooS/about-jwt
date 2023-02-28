12d7c9c4a919282a33f80a7fb83c7057d71ec790
Seungwoo, Joo <thisis.joos@gmail.com>
2023-02-28 21:32:02 +0900

    feat: 회원가입

---
+++ src/main/java/thisis/joos/jwt/dto/AuthorityDto.java
@@ -0,0 +1,12 @@
+package thisis.joos.jwt.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthorityDto {
+    private String authorityName;
+}
